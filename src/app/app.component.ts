44ff66f0f43cc248b8518d1d7f20b3e9a7f5f5e3
Almenara <alberto.almenara@gmail.com>
2025-04-02 19:29:36 +0200

    ajustes generales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './core/header/header.component';
-import { FooterComponent } from './core/footer/footer.component';
-import { MainComponent } from "./core/main/main.component";
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MainComponent } from "./layout/main/main.component";
 
 @Component({
   selector: 'app-root',
