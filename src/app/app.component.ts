0c2f3d94e4a1efba762a133f02bb8dfc3c3aef2c
Almenara <alberto.almenara@gmail.com>
2025-04-02 14:55:25 +0200

    header maquetado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { MainComponent } from "./core/main/main.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainComponent],
   standalone: true,
+  imports: [HeaderComponent, FooterComponent, MainComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'show-me-shows';
 }
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  
+  public isMenuOpen: WritableSignal<boolean> = signal(true);
+
+  public toggleMenu(): void {
+    this.isMenuOpen.update((value: boolean) => !this.isMenuOpen());
+  }
 
 }
